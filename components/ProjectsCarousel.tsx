be95b5fb37e6b982f7ccf6bce18d9cc31e8b27a3
Karman Singh <kswgeneral@gmail.com>
2026-03-22 17:15:51 +0530

    Update project entries in ProjectsCarousel
    Replaced existing projects with new entries in the ProjectsCarousel component.

--- components/ProjectsCarousel.tsx
+++ components/ProjectsCarousel.tsx
@@ -9,10 +9,11 @@ export function ProjectsCarousel({ scrollYProgress, index }: { scrollYProgress: 
     const dummyProjects = [
         { id: 1, title: 'desktop assistant', description: 'a desktop search feature which can help you browse through your files, folder, help you search on google, stackoverflow, yt and gemini with simple commands', githubLink: 'https://github.com/Karman-singh15/desktop-assistant', liveLink: 'https://x.com/KarmanSingh1505/status/1928883025594490939' },
         { id: 2, title: 'explainer ai', description: 'a extension where if you ask a doubt to the llm it summarizes the answer there instead of making a new chat so you can keep the explainations at one place rather than scrollin back and forth', githubLink: 'https://github.com/Karman-singh15/explainer-ai', liveLink: '' },
-        { id: 3, title: 'news', description: 'a news app where you can see the articles of right,left and centrist wing to make your own informed desicion', githubLink: 'https://github.com/Karman-singh15/news', liveLink: '' },
-        { id: 4, title: 'arc', description: 'a app where you can track your progress in the gym,diet and personal goals', githubLink: 'https://github.com/Karman-singh15/arc', liveLink: '' },
-        { id: 5, title: 'driver name updater', description: 'a website which was made with the sole purpose of making my moms life simpler', githubLink: 'https://github.com/Karman-singh15/driver-name-updater', liveLink: 'https://driver-name-updater.vercel.app/' },
-        { id: 6, title: 'amul protein notifier', description: 'a code that scrapes the amul website to tell me the availability of its protein products through whatsapp', githubLink: 'https://github.com/Karman-singh15/amul-protein-notifier', liveLink: '' },
+        { id: 3, title: 'bot arena', description: 'Built a multi-agent trading simulator combining behavioral modeling and machine learning to analyze how economic background affects financial decisions.', githubLink: 'https://github.com/Karman-singh15/bot-arena', liveLink: '' },
+        { id: 4, title: 'news', description: 'a news app where you can see the articles of right,left and centrist wing to make your own informed desicion', githubLink: 'https://github.com/Karman-singh15/news', liveLink: '' },
+        { id: 5, title: 'arc', description: 'a app where you can track your progress in the gym,diet and personal goals', githubLink: 'https://github.com/Karman-singh15/arc', liveLink: '' },
+        { id: 6, title: 'driver name updater', description: 'a website which was made with the sole purpose of making my moms life simpler', githubLink: 'https://github.com/Karman-singh15/driver-name-updater', liveLink: 'https://driver-name-updater.vercel.app/' },
+        
 
     ];
 
